1bba9d440b4eae4955b5268b4b5e413a584665ed
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2021-11-12 07:26:43 -0700

    Variable and String concatenation

--- 100_days_coding_challenge.py
+++ 100_days_coding_challenge.py
@@ -1,1 +1,18 @@
+# DAY 1
+
+# Print function
+print("Hello world!\nHello world!\nHello world!\n")
+print("Hello" + " " + "Finbarr")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# INPUT FUNCTION
+print("Hello" + " " + input("What is your name?"))
+name = input("What is your name?")
+print(len(name))
+
+
 
---
+++ Variable_challenge.py
@@ -0,0 +1,21 @@
+a = input("a:")
+b = input("b:")
+# Swapping of two numbers
+temp = a
+a=b
+b=temp
+print(a,b)
+
+input = 2
+print(input)
+
+# Create a greeting for your program
+print("Hello World"+ " "+ "Welcome to my Youtube Channel")
+
+# Ask the user for the city that they grew up in
+city =input("What city did you grow up in ?\n")
+
+name = input("What is your pet name?\n")
+
+print("I grew up in "+city+" "+"and my pet name is"+" "+name)
+
