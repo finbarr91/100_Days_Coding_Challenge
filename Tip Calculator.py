ddf3bfb571e2131d03766ea89234969a973c0f6d
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2021-11-13 16:37:01 -0700

    Day 3 Code challenge

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -5,10 +5,9 @@ percentage_tip = int(input("What percentage tip would you like to give? 10,12 or
 number_of_people_splitting_the_bill = int(input('How many people to split the bill?'))
 
 # Calculating the percentage tip with respect 10,12,15 percentage
-tip = percentage_tip/100
-total_bill_with_tip = total_bill+tip
-
+total_bill_with_tip = percentage_tip/100*total_bill + total_bill
 amount_each_person_has_to_pay = total_bill_with_tip/number_of_people_splitting_the_bill
+
 final_amount = round(amount_each_person_has_to_pay,2)
 final_amount = "{:.2f}".format(amount_each_person_has_to_pay)
 
